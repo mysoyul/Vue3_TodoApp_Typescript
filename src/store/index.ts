f9e9e8a665399eefc39a3906d3b70f917efdfa22
mysoyul <vega2k_@naver.com>
2024-04-05 14:48:55 +0900

    module level2

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,14 @@
 import { createStore, createLogger } from "vuex"
+import { moduleTodo, ModuleTodoState } from './modules/moduleTodo'
+
+export interface RootState {
+    moduleTodo: ModuleTodoState
+}
 
 export const store = createStore({
     plugins: process.env.NODE_ENV === 'development' ?
-        [createLogger()] : [],    
-
-
+        [createLogger()] : [],
+    modules: {
+        moduleTodo
+    }
 })
