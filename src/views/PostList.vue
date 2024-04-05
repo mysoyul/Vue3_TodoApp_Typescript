af9525796f6c694d3b6d102b74b2afb62fc464cf
mysoyul <vega2k_@naver.com>
2024-04-05 17:13:38 +0900

    PostList update

--- src/views/PostList.vue
+++ src/views/PostList.vue
@@ -1,6 +1,49 @@
 <template>
-    <div>
-        <h1>Posts</h1>
-        <router-view></router-view>
-    </div>
+    <div class="list row">
+        <div class="col-md-12">
+            <h1 class="text-center">Posts</h1>
+                <h3 v-if="loadingStatus" class="text-center">Loading...</h3>
+                <div v-else>
+                    <ul class="list-group">
+                        <li v-for="post in posts" :key="post.id" class="list-group-item">
+                            <router-link :to="{ name: 'post', params: { id: post.id } }" class="link-primary">
+                                [ID: {{ post.id }}] {{ summary(post.title) }}
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="container">
+                <router-view></router-view>
+            </div>
+        </div>
 </template>
+
+<script lang="ts" setup>
+import { useStore } from "vuex"
+import { onMounted, computed } from "vue"
+
+const store = useStore()
+
+const posts = computed(() => store.state.modulePost.posts)
+const loadingStatus = computed(() => store.state.modulePost.loadingStatus)
+
+onMounted(async () => {
+    try {
+        await fetchData();
+    } catch (error) {
+        console.log(error)
+    } 
+});
+
+const fetchData = () => {
+    store.dispatch("modulePost/loadPosts")
+};
+
+const summary = (val: string) => {
+    if (typeof val === "string") {
+        return val.substring(0, 10) + "...";
+    }
+    return val;
+};
+</script>
