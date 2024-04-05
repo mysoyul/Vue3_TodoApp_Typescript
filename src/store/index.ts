9c624c2b2f743b931b49b01e933eaf64510d2ae1
mysoyul <vega2k_@naver.com>
2024-04-05 13:36:48 +0900

    axios 사용

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,31 +1,37 @@
 import { createStore, createLogger } from "vuex"
-import TodoItem from "@/types/TodoItem";
+import axios from "axios"
 
-const storage = {
-    fetch() {
-        const arr = [];
-        if (localStorage.length > 0) {
-            for (let i = 0; i < localStorage.length; i++) {
-                const storageKey = localStorage.key(i) as string;
-                const itemJson = localStorage.getItem(storageKey) as string | null
-                if (itemJson) {
-                    arr.push(JSON.parse(itemJson))
-                }
-            }
-        }
-        return arr;
-    }
-}
+import TodoItem from "@/types/TodoItem";
+import http from "@/common/http-common"
 
 export type State = { todoItems: TodoItem[] };
 
-const state: State = { todoItems: storage.fetch() };
+const state: State = { todoItems: [] };
 
 export const store = createStore({
     plugins: process.env.NODE_ENV === 'development' ?
         [createLogger()] : [],
     state,
+    actions: {
+        loadTodoItems({ commit }) {
+            http
+                .get('/todos')
+                .then(r => r.data)
+                .then(items => commit('setTodoItems', items))
+                .catch(error => {
+                    if (axios.isAxiosError(error)) {
+                        console.log(error?.response?.status +
+                            ' : ' + error.message)
+                    } else {
+                        console.error(error);
+                    }
+                });
+        }, //loadTodoItems
+    },
     mutations: {
+        setTodoItems(state: State, items: TodoItem[]) {
+            state.todoItems = items;
+        },
         addTodo(state: State, todoItem: string) {
             const obj = { completed: false, item: todoItem };
             localStorage.setItem(todoItem, JSON.stringify(obj));
